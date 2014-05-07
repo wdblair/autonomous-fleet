2a1890de3f3fe3aa4899242322e1e41b5fe71a18
William Blair <wdblair@bu.edu>
2014-05-07 11:13:17 -0400

    Touched up install.sh script, improved readme

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 Final Project CS640 README
 ==========================
 
-This repo contains the code needed to run
+This repo contains the code needed to run our final project.
 
 Dependencies
 ============
 
 - Matlab 2013b 
-- FlightGear (any recent version from the website should be fine)
+- FlightGear (v 2.12+)
+
+Instructions
+============
+
+In the single-agent directory, run the install.sh file to install
+all the necessary files to the FlightGear directory. You will need
+to find where your FlightGear data directory is before hand and edit
+the FG_ROOT variable in install.sh.
+
+Once FlightGear is installed, you can open up Matlab in the planner
+directory and run "exampleRun.m" which will demonstrate our Matlab
+system starting and guiding the single agent through takeoff and cruising
+to a destination airport.
+
+Unfortunately, if you're not running this on a Mac, the run-fg.sh script
+may not work as we needed to hardcode the location of the Flightgear executable.
+To replace it with the path your Flightgear is located, run
+
+    which fgfs
+
+and put the output into planner/run-fg.sh.
--- single-agent/install.sh
+++ single-agent/install.sh
@@ -6,18 +6,23 @@
 # On Mac: You don't need to be root to run this
 
 # For Linux (the path varies)
-# export FG_ROOT=/usr/share/flightgear/data
+# FG_ROOT=/usr/share/games/flightgear
 
 # For MacOS X
-# export FG_ROOT=/Applications/Flightgear.app/Contents/Resources/data
+# FG_ROOT=/Applications/Flightgear.app/Contents/Resources/data
 
 FG_ROOT=/usr/share/games/flightgear
 
-cp ./777-autopilot-uav.xml $FG_ROOT/Aircraft/777-200/Systems/777-autopilot-uav.xml
-cp ./uav.nas $FG_ROOT/Aircraft/777-200/Nasal/uav.nas
+cp ./777-autopilot-uav.xml $FG_ROOT/Aircraft/777/Systems/777-autopilot-uav.xml
+cp ./uav.nas $FG_ROOT/Aircraft/777/Nasal/uav.nas
 
+cp ./actuator_protocol.xml $FG_ROOT/Protocol/
+cp ./sensor_protocol.xml $FG_ROOT/Protocol/
+
+# Add our Nasal code into the 777 configuration.
 sed -i '/<b777>/ a\
-\<file\>Aircraft/777/Nasal/uav.nas\<\/file\>' $FG_ROOT/Aircraft/777-200/777-200ER-set.xml
+\<file\>Aircraft/777/Nasal/uav.nas\<\/file\>' $FG_ROOT/Aircraft/777/777-set-common.xml
 
+# Add our custom autopilot to the 777 autopilots.
 sed -i '/systems.xml\<\/path\>/ a\
-\<autopilot\><path\>Aircraft/777/Systems/777-autopilot-uav.xml\<\/path\>\<\/autopilot\>' $FG_ROOT/Aircraft/777-200/777-200ER-set.xml
+\<autopilot\><path\>Aircraft/777/Systems/777-autopilot-uav.xml\<\/path\>\<\/autopilot\>' $FG_ROOT/Aircraft/777/777-set-common.xml
